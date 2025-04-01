48cec6e458d711596dbbb93e11e9248891683ff8
Jon-Felipe <rodriguesjf7@gmail.com>
2025-04-01 18:10:44 +0200

    button component init

---
+++ components/UI/Button.tsx
@@ -0,0 +1,8 @@
+import { StyleSheet, Button as UIButton } from 'react-native';
+import React from 'react';
+
+export default function Button() {
+  return <UIButton title='Click Me' />;
+}
+
+const styles = StyleSheet.create({});
