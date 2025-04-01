f3fcd9be485d7369c003f7871a0f22f165980205
Jon-Felipe <rodriguesjf7@gmail.com>
2025-04-01 17:59:12 +0200

    global styles constants

---
+++ styles.ts
@@ -0,0 +1,16 @@
+export const GlobalStyles = {
+  colors: {
+    primary50: '#FDB286',
+    primary100: '#FC8B4A',
+    primary200: '#FC7222',
+    primary400: '#FB650E',
+    primary500: '#F35B04',
+    primary700: '#DD5303',
+    primary800: '#C94C03',
+    error50: '#F5968F',
+    error500: '#EA2B1F',
+    gray50: '#F6F6F6',
+    gray500: '#797979',
+    gray700: '#242424',
+  },
+};
