6caebebc167b893c605588e66fd07d2ce8753392
Jon-Felipe <rodriguesjf7@gmail.com>
2025-04-01 17:15:52 +0200

    input component init

---
+++ components/UI/Input.tsx
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View, TextInput } from 'react-native';
+import React from 'react';
+
+export default function Input() {
+  return (
+    <View>
+      <Text>Input Label</Text>
+      <TextInput />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
