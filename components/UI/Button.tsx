b0478c6b34ef66d745b5a7c8f27d36709421998c
Jon-Felipe <rodriguesjf7@gmail.com>
2025-04-01 18:19:00 +0200

    button component styling

--- components/UI/Button.tsx
+++ components/UI/Button.tsx
@@ -1,8 +1,29 @@
-import { StyleSheet, Button as UIButton } from 'react-native';
+import { Pressable, StyleSheet, Text, View } from 'react-native';
 import React from 'react';
 
+// extras
+import { GlobalStyles } from '../../styles';
+
 export default function Button() {
-  return <UIButton title='Click Me' />;
+  return (
+    <Pressable>
+      <View style={styles.button}>
+        <Text style={styles.buttonText}>Click Me</Text>
+      </View>
+    </Pressable>
+  );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  button: {
+    backgroundColor: GlobalStyles.colors.primary500,
+    borderRadius: 24,
+    padding: 14,
+  },
+  buttonText: {
+    color: 'white',
+    textAlign: 'center',
+    fontSize: 16,
+    fontWeight: '600',
+  },
+});
