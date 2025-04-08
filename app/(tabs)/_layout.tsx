62e9ac86ef784e34412da32388adab915ad94efd
Jon-Felipe <rodriguesjf7@gmail.com>
2025-04-08 20:30:00 +0200

    setup tabs navigation and initial tab routes

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,55 @@
+import { Tabs } from 'expo-router';
+import FontAwesome from '@expo/vector-icons/FontAwesome';
+
+type tabBarIcon = {
+  focused?: boolean;
+  color?: string;
+  size?: number;
+};
+
+export default function RootLayout() {
+  return (
+    <Tabs>
+      <Tabs.Screen
+        name='index'
+        options={{
+          title: 'Home',
+          headerShown: false,
+          tabBarIcon: ({ color }: tabBarIcon) => (
+            <FontAwesome size={28} name='home' color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name='discover'
+        options={{
+          title: 'Discover',
+          headerShown: false,
+          tabBarIcon: ({ color }: tabBarIcon) => (
+            <FontAwesome size={28} name='compass' color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name='favourites'
+        options={{
+          title: 'Favourite',
+          headerShown: false,
+          tabBarIcon: ({ color }: tabBarIcon) => (
+            <FontAwesome size={28} name='heart' color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name='profile'
+        options={{
+          title: 'Profile',
+          headerShown: false,
+          tabBarIcon: ({ color }: tabBarIcon) => (
+            <FontAwesome size={28} name='user' color={color} />
+          ),
+        }}
+      />
+    </Tabs>
+  );
+}
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,9 @@
 import { Text, View } from 'react-native';
 
-export default function Index() {
+export default function HomeScreen() {
   return (
     <View className='flex-1 justify-center items-center'>
-      <Text className='text-4xl'>test</Text>
+      <Text className='text-4xl'>HomeScreen</Text>
     </View>
   );
 }
