671da5e02fb2edef5beb3a05f8065913a2d75958
Jon-Felipe <rodriguesjf7@gmail.com>
2025-04-09 17:23:40 +0200

    temp link to auth screen

--- app/(logged-in)/(tabs)/index.tsx
+++ app/(logged-in)/(tabs)/index.tsx
@@ -1,9 +1,11 @@
+import { Link } from 'expo-router';
 import { Text, View } from 'react-native';
 
 export default function HomeScreen() {
   return (
     <View className='flex-1 justify-center items-center'>
       <Text className='text-4xl'>HomeScreen</Text>
+      <Link href={'/auth'}>Go to Auth Screen</Link>
     </View>
   );
 }
