d49d41acc02333584bf8e4522d2d5fc78eec395d
Jon-Felipe <rodriguesjf7@gmail.com>
2025-04-10 16:06:23 +0200

    created and displaying header component

--- app/(logged-in)/(tabs)/index.tsx
+++ app/(logged-in)/(tabs)/index.tsx
@@ -1,11 +1,14 @@
 import { Link } from 'expo-router';
 import { Text, View } from 'react-native';
 
+// components
+import Header from '@/components/home/Header';
+
 export default function HomeScreen() {
   return (
-    <View className='flex-1 justify-center items-center'>
-      <Text className='text-4xl'>HomeScreen</Text>
-      <Link href={'/auth'}>Go to Auth Screen</Link>
+    <View className='flex-1 bg-white p-8'>
+      <Header />
+      {/* <Link href={'/auth'}>Go to Auth Screen</Link> */}
     </View>
   );
 }
---
+++ components/home/Header.tsx
@@ -0,0 +1,20 @@
+import { View, Text, Image } from 'react-native';
+import React from 'react';
+import FontAwesome from '@expo/vector-icons/FontAwesome';
+
+export default function Header() {
+  return (
+    <View className='flex flex-row items-center justify-between'>
+      <View className='flex flex-row items-center gap-x-4'>
+        <Image
+          source={{ uri: 'https://i.pravatar.cc/150?img=6' }}
+          className='w-12 h-12 rounded-full'
+        />
+        <Text className='text-xl font-medium'>Hello, Jon 👋</Text>
+      </View>
+      <View className='border border-neutral-200 rounded-full p-2'>
+        <FontAwesome name='bell-o' size={24} />
+      </View>
+    </View>
+  );
+}
