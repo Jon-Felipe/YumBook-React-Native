966794b3f7ac393302ef416024f58f22e183017d
Jon-Felipe <rodriguesjf7@gmail.com>
2025-04-11 15:32:08 +0200

    displaying popular recipes on home page

--- app/(logged-in)/(tabs)/index.tsx
+++ app/(logged-in)/(tabs)/index.tsx
@@ -1,13 +1,17 @@
-import { Link } from 'expo-router';
-import { Text, View } from 'react-native';
+// import { Link } from 'expo-router';
+import { View } from 'react-native';
 
 // components
 import Header from '@/components/home/Header';
+import PopularRecipes from '@/components/home/PopularRecipes';
 
 export default function HomeScreen() {
   return (
-    <View className='flex-1 bg-white p-8'>
+    <View className='flex-1 bg-white p-5'>
       <Header />
+      <View className='mt-8'>
+        <PopularRecipes />
+      </View>
       {/* <Link href={'/auth'}>Go to Auth Screen</Link> */}
     </View>
   );
