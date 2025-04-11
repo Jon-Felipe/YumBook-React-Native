4c4144821be1b59542523b6123d1234908f91674
Jon-Felipe <rodriguesjf7@gmail.com>
2025-04-11 19:17:33 +0200

    displaying list of categories home screen

--- app/(logged-in)/(tabs)/index.tsx
+++ app/(logged-in)/(tabs)/index.tsx
@@ -1,9 +1,11 @@
 // import { Link } from 'expo-router';
-import { View } from 'react-native';
+import { ScrollView, Text, View } from 'react-native';
 
 // components
 import Header from '@/components/home/Header';
 import PopularRecipes from '@/components/home/PopularRecipes';
+import { dummy_categories } from '@/data/dummy_categories';
+import Pill from '@/components/ui/Pill';
 
 export default function HomeScreen() {
   return (
@@ -11,6 +13,19 @@ export default function HomeScreen() {
       <Header />
       <View className='mt-8'>
         <PopularRecipes />
+      </View>
+      <View className='mt-8'>
+        <View className='flex flex-row items-center justify-between mb-2'>
+          <Text className='text-lg font-semibold'>Categories</Text>
+          <Text className='text-neutral-400'>See All</Text>
+        </View>
+        <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+          {dummy_categories.map((category) => (
+            <View key={category.id} className='mr-2'>
+              <Pill text={category.name} icon={category.imageUrl} />
+            </View>
+          ))}
+        </ScrollView>
       </View>
       {/* <Link href={'/auth'}>Go to Auth Screen</Link> */}
     </View>
