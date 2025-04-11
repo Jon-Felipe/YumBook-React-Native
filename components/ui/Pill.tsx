f8bed508ae97461e873cd07c8e81476916cf376b
Jon-Felipe <rodriguesjf7@gmail.com>
2025-04-11 19:17:13 +0200

    pill component init

---
+++ components/ui/Pill.tsx
@@ -0,0 +1,16 @@
+import { View, Text, Image } from 'react-native';
+import React from 'react';
+
+type PillProps = {
+  text: string;
+  icon?: any;
+};
+
+export default function Pill({ text, icon }: PillProps) {
+  return (
+    <View className='flex flex-row items-center justify-between gap-x-2 border border-neutral-200 rounded-full px-4 py-2'>
+      <Image source={icon} className='w-4 h-4' />
+      <Text>{text}</Text>
+    </View>
+  );
+}
