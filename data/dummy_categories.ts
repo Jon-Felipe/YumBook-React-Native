f0c416af39b073bce7e8e6c7823574bcf0c2c5fe
Jon-Felipe <rodriguesjf7@gmail.com>
2025-04-11 19:00:00 +0200

    displaying list of dummy categories

---
+++ data/dummy_categories.ts
@@ -0,0 +1,44 @@
+import { Category } from '@/utils/types';
+
+export const dummy_categories: Category[] = [
+  {
+    id: '1',
+    name: 'Breakfast',
+    description: 'Start your day with these morning favourites.',
+    imageUrl: 'breakfast',
+  },
+  {
+    id: '2',
+    name: 'Lunch',
+    description: 'Midday meals to keep you going',
+    imageUrl: 'lunch',
+  },
+  {
+    id: '3',
+    name: 'Burgers',
+    description:
+      'Juicy, stacked, and packed with flavor — from classic beef to creative plant-based patties.',
+    imageUrl: 'burger',
+  },
+  {
+    id: '4',
+    name: 'Noodles',
+    description:
+      'Slurp-worthy dishes from stir-fry to soup, spanning Asian-inspired flavors and beyond.',
+    imageUrl: 'noodles',
+  },
+  {
+    id: '5',
+    name: 'Pasta',
+    description:
+      'Comfort food at its finest — creamy, saucy, and carb-loving classics from Italy and more.',
+    imageUrl: 'pasta',
+  },
+  {
+    id: '6',
+    name: 'Pizza',
+    description:
+      'Cheesy, crispy, and endlessly customizable — a slice of heaven for every taste.',
+    imageUrl: 'pizza',
+  },
+];
