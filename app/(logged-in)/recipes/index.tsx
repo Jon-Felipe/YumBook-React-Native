da74ff3f3ca3845736e14a598d604d5c54eeb687
Jon-Felipe <rodriguesjf7@gmail.com>
2025-04-12 16:13:44 +0200

    added 2 new screens, recipes and single recipe screen init

---
+++ app/(logged-in)/recipes/index.tsx
@@ -0,0 +1,32 @@
+import { View, Text } from 'react-native';
+import React from 'react';
+import { useLocalSearchParams } from 'expo-router';
+
+// extras
+import { dummy_recipes } from '@/data/dummy_recipes';
+
+export default function RecipesScreen() {
+  const { categoryId } = useLocalSearchParams();
+
+  const recipes = dummy_recipes.filter((recipe) =>
+    recipe.categoryId.includes(categoryId.toString())
+  );
+
+  if (recipes.length <= 0) {
+    return (
+      <View className='flex flex-1 items-center justify-center bg-white'>
+        <Text className='text-xl font-semibold'>
+          No recipes found for this category
+        </Text>
+      </View>
+    );
+  }
+
+  return (
+    <View>
+      {recipes.map((recipe) => (
+        <Text key={recipe.id}>{recipe.title}</Text>
+      ))}
+    </View>
+  );
+}
