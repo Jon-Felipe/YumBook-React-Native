0879a312d2686b1d1e796e79b657d9e07ac4cad3
Jon-Felipe <rodriguesjf7@gmail.com>
2025-04-12 11:55:48 +0200

    refactored home screen to display flat list with header content

--- app/(logged-in)/(tabs)/index.tsx
+++ app/(logged-in)/(tabs)/index.tsx
@@ -1,19 +1,28 @@
 // import { Link } from 'expo-router';
-import { ScrollView, Text, View } from 'react-native';
+import { FlatList, ScrollView, Text, View } from 'react-native';
 
 // components
 import Header from '@/components/home/Header';
 import PopularRecipes from '@/components/home/PopularRecipes';
-import { dummy_categories } from '@/data/dummy_categories';
 import Pill from '@/components/ui/Pill';
+import RecipeCard from '@/components/shared/RecipeCard';
+
+// extras
+import { dummy_categories } from '@/data/dummy_categories';
+import { dummy_recipes } from '@/data/dummy_recipes';
 
 export default function HomeScreen() {
-  return (
-    <View className='flex-1 bg-white p-5'>
+  const renderHeader = () => (
+    <View className='bg-white'>
+      {/* header */}
       <Header />
+
+      {/* popular recipe scroll list */}
       <View className='mt-8'>
         <PopularRecipes />
       </View>
+
+      {/* category pill scroll list */}
       <View className='mt-8'>
         <View className='flex flex-row items-center justify-between mb-2'>
           <Text className='text-lg font-semibold'>Categories</Text>
@@ -27,7 +36,24 @@ export default function HomeScreen() {
           ))}
         </ScrollView>
       </View>
+
+      {/* list of recipes title */}
+      <View className='mt-8 mb-4'>
+        <Text className='text-xl font-semibold'>Recipes For You</Text>
+      </View>
       {/* <Link href={'/auth'}>Go to Auth Screen</Link> */}
+    </View>
+  );
+
+  return (
+    <View className='p-5 bg-white'>
+      <FlatList
+        data={dummy_recipes}
+        renderItem={({ item }) => <RecipeCard recipe={item} />}
+        keyExtractor={(item) => item.id}
+        ListHeaderComponent={renderHeader}
+        showsVerticalScrollIndicator={false}
+      />
     </View>
   );
 }
