0e4f9dea09179eadb5abd896451b9a94581684b4
Jon-Felipe <rodriguesjf7@gmail.com>
2025-04-12 11:24:31 +0200

    conditional check for icon pill component

--- components/ui/Pill.tsx
+++ components/ui/Pill.tsx
@@ -9,7 +9,7 @@ type PillProps = {
 export default function Pill({ text, icon }: PillProps) {
   return (
     <View className='flex flex-row items-center justify-between gap-x-2 border border-neutral-200 rounded-full px-4 py-2'>
-      <Image source={icon} className='w-4 h-4' />
+      {icon && <Image source={icon} className='w-4 h-4' />}
       <Text>{text}</Text>
     </View>
   );
