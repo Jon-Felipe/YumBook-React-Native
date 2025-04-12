58afcf7f8c15e1d2e5fae74a6f2b94a0fc1d5b66
Jon-Felipe <rodriguesjf7@gmail.com>
2025-04-12 16:14:24 +0200

    navigating to recipes for selected category

--- app/(logged-in)/category.tsx
+++ app/(logged-in)/category.tsx
@@ -1,28 +1,38 @@
-import { View, Text, Image } from 'react-native';
+import { View, Text, Image, Pressable } from 'react-native';
 import React from 'react';
+import { useRouter } from 'expo-router';
 
 // extras
 import { dummy_categories } from '@/data/dummy_categories';
 
 export default function CategoryScreen() {
+  const { navigate } = useRouter();
+
   return (
     <View className='flex flex-1 bg-white'>
       <View className='flex flex-row items-center justify-center flex-wrap gap-6 p-5'>
         {dummy_categories.map((category) => (
-          <View
+          <Pressable
             key={category.id}
-            className='relative flex items-center justify-center rounded-full w-28 h-28 overflow-hidden'
+            onPress={() =>
+              navigate({
+                pathname: './recipes',
+                params: { categoryId: category.id },
+              })
+            }
           >
-            <Image
-              source={category.image! as any}
-              className='w-full h-full'
-              resizeMode='cover'
-            />
-            <View className='absolute top-0 left-0 right-0 bottom-0 bg-black opacity-25' />
-            <Text className='absolute text-white text-lg font-semibold text-center'>
-              {category.name}
-            </Text>
-          </View>
+            <View className='relative flex items-center justify-center rounded-full w-28 h-28 overflow-hidden'>
+              <Image
+                source={category.image! as any}
+                className='w-full h-full'
+                resizeMode='cover'
+              />
+              <View className='absolute top-0 left-0 right-0 bottom-0 bg-black opacity-25' />
+              <Text className='absolute text-white text-lg font-semibold text-center'>
+                {category.name}
+              </Text>
+            </View>
+          </Pressable>
         ))}
       </View>
     </View>
